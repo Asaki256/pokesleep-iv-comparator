02bbcc9f36d06ea66e6299e4f6e52ed11ec676b7
Masaki <ryota423.jp@icloud.com>
2025-10-23 21:13:54 +0900

    Nextへの移行

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/components/Header";
+import Description from "@/components/Description";
+import Rank from "@/components/Rank";
+import Result from "@/components/Result";
+import Search from "@/components/Search";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Description />
+      <Rank />
+      <Result />
+      <Search />
+    </>
+  );
+}
