1552607e1a413544436ed7ca46834a7b466e3e3e
Masaki <ryota423.jp@icloud.com>
2025-10-24 09:12:47 +0900

    ビルドファイルが自動で修正された

---
+++ .next/dev/static/chunks/Develop_pokesleep-iv-comparator_app_page_tsx_f28fb9da._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/173f1_0a736d4e._.js",
+  "static/chunks/Develop_pokesleep-iv-comparator_src_31864603._.js"
+],
+    source: "dynamic"
+});
