ad5555f12771dfec7737e7da31ec99a884ea34e4
Masaki <ryota423.jp@icloud.com>
2025-10-24 09:12:01 +0900

    ポケモン名のコンポ構造修正

---
+++ src/components/PokeNameCombobox.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+const frameworks = [
+  {
+    value: "pikachu",
+    label: "ピカチュウ",
+  },
+  {
+    value: "raichu",
+    label: "ライチュウ",
+  },
+  {
+    value: "pupurin",
+    label: "ププリン",
+  },
+  {
+    value: "kyatapi-",
+    label: "キャタピー",
+  },
+  {
+    value: "bangirasu",
+    label: "バンギラス",
+  },
+];
+
+interface Props {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+export function Combobox({ value, onChange }: Props) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[400px] justify-between"
+        >
+          {value
+            ? frameworks.find(
+                (framework) => framework.value === value
+              )?.label
+            : "ポケモン名を選択"}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[400px] p-0">
+        <Command>
+          <CommandInput
+            placeholder="ポケモン名を検索"
+            className="h-9"
+          />
+          <CommandList>
+            <CommandEmpty>No framework found.</CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.label}
+                  onSelect={(currentValue) => {
+                    const selectedFramework =
+                      frameworks.find(
+                        (f) => f.label === currentValue
+                      );
+                    onChange(
+                      selectedFramework?.value === value
+                        ? ""
+                        : selectedFramework?.value || ""
+                    );
+                    setOpen(false);
+                  }}
+                >
+                  {framework.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === framework.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
